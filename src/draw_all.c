cb61437b2fff200fe955b4b6d7a143ceed2205a1
Martin <martin.d-herouville@epitech.eu>
2022-04-27 13:24:05 +0200

    (+) display fps

--- src/draw_all.c
+++ src/draw_all.c
@@ -41,9 +41,25 @@ spritesheet_t *spritesheet)
     }
 }
 
-void draw_text(rpg_t *rpg)
+void draw_all_text(rpg_t *rpg)
 {
+    float zoom = get_zoom(rpg->begin.view.view);
+    sfVector2f pos_l = {rpg->begin.view.center.x - ((WIDTH / 2) * zoom) + (10 * zoom), rpg->begin.view.center.y + ((HEIGHT / 2) * zoom) - (40 * zoom)};
+    sfVector2f pos = {pos_l.x + (70 * zoom), pos_l.y};
+    sfVector2f scale = {0.3 * zoom, 0.3 * zoom};
+
     send_chat_bubble_soldiers(rpg);
+    if (get_clock_time(rpg->begin.fps_disp.display_clock) >= SECOND_TO_MICRO(0.2)) {
+        sfText_setString(rpg->begin.fps_disp.fps_text, my_itoa(rpg->begin.fps_disp.fps));
+        sfClock_restart(rpg->begin.fps_disp.display_clock);
+    }
+    sfText_setPosition(rpg->begin.fps_disp.fps_text, pos);
+    sfText_setScale(rpg->begin.fps_disp.fps_text, scale);
+    sfText_setPosition(rpg->begin.fps_disp.legende, pos_l);
+    sfText_setScale(rpg->begin.fps_disp.legende, scale);
+    write_text(rpg->begin.window, rpg->begin.fps_disp.fps_text);
+    write_text(rpg->begin.window, rpg->begin.fps_disp.legende);
+
 }
 
 void draw_all(rpg_t *rpg)
@@ -54,7 +70,7 @@ void draw_all(rpg_t *rpg)
     sfRenderWindow_drawSprite(rpg->begin.window,
     rpg->begin.sprite, NULL);
     draw_spritesheets(&rpg->begin, rpg->spritesheet);
-    draw_text(rpg);
+    draw_all_text(rpg);
     if (rpg->screen[SC_INVENTORY].active)
         draw_inventory(rpg->player_stats.inventory,
         &rpg->begin, rpg->spritesheet);
--- src/myrpg.c
+++ src/myrpg.c
@@ -7,14 +7,32 @@
 
 #include "../include/rpg.h"
 
+int get_fps(rpg_t *rpg)
+{
+    rpg->begin.fps_disp.time = sfClock_getElapsedTime(rpg->begin.fps_disp.clock).microseconds;
+    rpg->begin.fps_disp.fps = 1 / (rpg->begin.fps_disp.time / 1000000);
+    sfClock_restart(rpg->begin.fps_disp.clock);
+    return (rpg->begin.fps_disp.fps);
+}
+
 static void big_loop(rpg_t *rpg, sfColor *oui)
 {
     *oui = my_rgb(*oui);
+    get_fps(rpg);
     my_events(rpg);
     if ((rpg->all_events.q || rpg->all_events.c) && rpg->all_events.ctrl)
         sfRenderWindow_close(rpg->begin.window);
     // for (int i = 0; i < NBR_SP; ++i)
     //     sfSprite_setColor(rpg->spritesheet[i].sprite, *oui);
+    if (rpg->all_events.page_down) {
+        set_zoom(rpg->begin.view.view, get_zoom(rpg->begin.view.view) + 0.1);
+        rpg->all_events.page_down = false;
+    }
+    if (rpg->all_events.page_up) {
+        set_zoom(rpg->begin.view.view, get_zoom(rpg->begin.view.view) - 0.1);
+        rpg->all_events.page_up = false;
+    }
+    printf("get_zoom() = %0.2f\n", get_zoom(rpg->begin.view.view));
     move_all_fps_independant(rpg);
     execute_all(rpg);
     check_click_buttons(rpg);
@@ -30,7 +48,7 @@ void myrpg(void)
     init_all(rpg);
     if (!rpg->begin.window || !rpg->begin.framebuffer)
         return;
-    sfWindow_setFramerateLimit((sfWindow *)rpg->begin.window, 120);
+    sfWindow_setFramerateLimit((sfWindow *)rpg->begin.window, 1000);
     rpg->begin.fps.clock = sfClock_create();
     play_sound(rpg->sound.sound_list[SOUND_MENU].sound, rpg->sound.volume);
     toggle_cursor(rpg->begin.window, false);
