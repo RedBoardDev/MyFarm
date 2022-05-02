90feb879a9294b21295d06e973cd98529910c2df
RedBoardDev <ott.thomas68@gmail.com>
2022-05-02 17:35:13 +0200

    (|) remake engine for quests and remake quest soldier

--- src/init/init_quests.c
+++ src/init/init_quests.c
@@ -11,12 +11,14 @@ void init_quests(rpg_t *rpg)
 {
     rpg->quest = malloc(sizeof(quest_t) * NBR_QUEST);
     rpg->quest[QUEST_SOLDIER] = (quest_t) {.step = 0};
+    rpg->quest[QUEST_SOLDIER].active = 0;
     rpg->quest[QUEST_SOLDIER].pos_dialog = (sfVector2f){120, 230};
     rpg->quest[QUEST_SOLDIER].scale = (sfVector2f){0.08, 0.08};
     rpg->quest[QUEST_SOLDIER].dialog =
     create_text((init_text_t){30, "\0", sfBlack,
     {92, 210}, "assets/fonts/NotoSerif-Regular.ttf"});
     rpg->quest[QUEST_SELLER] = (quest_t) {.step = 0};
+    rpg->quest[QUEST_SELLER].active = 0;
     rpg->quest[QUEST_SELLER].pos_dialog = (sfVector2f){1020, 40};
     rpg->quest[QUEST_SELLER].scale = (sfVector2f){0.08, 0.08};
     rpg->quest[QUEST_SELLER].dialog =
