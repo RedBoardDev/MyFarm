54b4c3db441dc50d2dbf7df5a80a5fb24dc97557
Martin <martin.d-herouville@epitech.eu>
2022-05-07 11:30:03 +0200

    (|) modified screens gameplay

--- src/utils/check_screens.c
+++ src/utils/check_screens.c
@@ -19,7 +19,8 @@ bool is_in_gameplay(rpg_t *rpg)
     || rpg->screen[SC_GROTTE].active || rpg->screen[SC_INVENTORY].active
     || rpg->screen[SC_CEMETERY].active ||
     rpg->screen[SC_VICTORY_CEMETERY].active || rpg->screen[SC_TENTE].active
-    || rpg->screen[SC_TAVERNE].active || rpg->screen[SC_JAIL].active);
+    || rpg->screen[SC_TAVERNE].active || rpg->screen[SC_JAIL].active ||
+    rpg->screen[SC_GUI_SHOP].active || rpg->screen[SC_VICTORY_GROTTE].active);
 }
 
 int get_current_screen(rpg_t *rpg)
